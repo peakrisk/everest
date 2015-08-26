f02d398424ac1ab3678c7f84040cfe57381350c7
John Gill <swfiua@gmail.com>
2015-08-26 16:54:46 -0300

    initial version

---
+++ everest.py
@@ -0,0 +1,21 @@
+"""
+
+================
+Event Generation
+================
+
+Starting at the top, with event generation, everest seems like the
+obvious choice.
+
+Trivia
+======
+
+So Everest is the highest, at least above sea level.  As far as I am
+aware there is no danger of it losing that title any time soon.  Maybe
+an asteroid hit might have something to say about it.
+
+LICENSE
+=======
+
+FIXME: add a license file
+GPL v 3
