682c7b422d4a230fb3374e383877dcf4e0903b22
John Gill <swfiua@gmail.com>
2016-03-08 14:34:15 -0400

    package is everest_climber

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = '0.0.1'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 # get the dependencies and installs
@@ -18,7 +18,7 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
 
 setup(
-    name='everest',
+    name='everest_climber',
     version=__version__,
     description='Simple event simulation framework',
     long_description=long_description,
